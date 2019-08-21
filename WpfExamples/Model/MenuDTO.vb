7cac56ae92958b74f666d16dcb1ca619a2fea411
ncxn <ncxn@outlook.com>
2019-08-21 17:50:11 +0700

    test

---
+++ WpfExamples/Model/MenuDTO.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Data
+Imports WpfExamples
+
+Public Class MenuDTO
+    Private _Header As String
+    Private _Parent As String
+    Private _Command As ICommand
+    Private _subMenu As ObservableCollection(Of MenuDTO)
+    Public Property Header As String
+        Get
+            Return _Header
+        End Get
+        Set(value As String)
+            _Header = value
+        End Set
+    End Property
+    Public Property Parent As String
+        Get
+            Return _Parent
+        End Get
+        Set(value As String)
+            _Parent = value
+        End Set
+    End Property
+    Public Property Command As ICommand
+        Get
+            Return _Command
+        End Get
+        Set(value As ICommand)
+            _Command = value
+        End Set
+    End Property
+
+    Public Property SubMenu As ObservableCollection(Of MenuDTO)
+        Get
+            Return _subMenu
+        End Get
+        Set(value As ObservableCollection(Of MenuDTO))
+            _subMenu = value
+        End Set
+    End Property
+
+    Public Shared Function GetAllMenuItems() As List(Of MenuDTO)
+
+        Dim dt As DataTable = DBHelper.GetInstance.GetDataTable("procGetAllControlsWithTypeMenu", CommandType.StoredProcedure)
+        Dim allItems As List(Of MenuDTO) = (From dr As DataRow In dt.Rows Select New MenuDTO With {
+                 .Header = dr(1),
+                .Parent = dr(2).ToString()
+            }).ToList()
+        Return allItems
+    End Function
+
+End Class
---
+++ WpfExamples/ViemModel/MenuItemVM.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Collections.ObjectModel
+Imports WpfExamples
+
+Public Class MenuItemVM
+    Inherits BaseVM
+
+    Private _children As ObservableCollection(Of MenuDTO)
+    Private _parent As ObservableCollection(Of MenuDTO)
+    Private _onSelected As ICommand
+
+    Public Sub New()
+        _parent = getParent()
+        _children = getChildren()
+    End Sub
+
+    Public Property Children As ObservableCollection(Of MenuDTO)
+        Get
+            Return _children
+        End Get
+        Set(ByVal value As ObservableCollection(Of MenuDTO))
+            _children = value
+            OnPropertyChanged(NameOf(Children))
+        End Set
+    End Property
+    Public Property Parent As ObservableCollection(Of MenuDTO)
+        Get
+            Return _parent
+        End Get
+        Set(ByVal value As ObservableCollection(Of MenuDTO))
+            _parent = value
+            OnPropertyChanged(NameOf(Parent))
+        End Set
+    End Property
+
+    Public ReadOnly Property OnSelected As ICommand
+        Get
+            If _onSelected Is Nothing Then
+                _onSelected = New RelayCommand(AddressOf OnItemSelected, AddressOf ItemCanBeSelected)
+            End If
+            Return _onSelected
+        End Get
+    End Property
+
+
+    Public Sub OnItemSelected(obj As Object)
+        MessageBox.Show("Click vào éo nào" + obj.ToString)
+    End Sub
+
+    Public Function ItemCanBeSelected(obj As Object) As Boolean
+        Return True
+    End Function
+
+    Private Function GetParent()
+        Dim listObj = MenuDTO.GetAllMenuItems.Where(Function(x) x.Parent Is String.Empty Or x.Parent Is Nothing)
+        Dim pr As ObservableCollection(Of MenuDTO) = New ObservableCollection(Of MenuDTO)(listObj)
+        For Each i In pr
+            Debug.WriteLine(i.Header.ToString)
+        Next
+        Return pr
+
+    End Function
+
+    Private Function GetChildren()
+        Dim listObj = MenuDTO.GetAllMenuItems.Where(Function(x) x.Parent IsNot Nothing)
+        Dim cr As ObservableCollection(Of MenuDTO) = New ObservableCollection(Of MenuDTO)(listObj)
+        Return cr
+    End Function
+End Class
